bdbe8fecb6e84583581b213f900aa77ceeff00d5
koro <koro.nn99@gmail.com>
2021-01-23 18:14:19 -0800

    showed indicator in Shops map

--- RamenShopApp/View/ShopsMapView.swift
+++ RamenShopApp/View/ShopsMapView.swift
@@ -12,27 +12,32 @@ struct ShopsMapView: View {
     @ObservedObject var viewModel: ShopsMapViewModel
     
     var body: some View {
-        VStack {
-            ZStack(alignment: .topTrailing) {
-                GoogleMapView(shopsMapVM: viewModel)
-                    .padding(5)
-                    .background(Color.blue)
-                
-                Button(action: {
-                    self.viewModel.loadShops()
-                }) {
-                    Text("Reload")
-                        .foregroundColor(.white)
-                        .padding(10)
-                        .background(Color.purple)
-                        .cornerRadius(13)
-                        .overlay(RoundedRectangle(cornerRadius: 13)
-                                    .stroke(Color.black, lineWidth: 2)
-                        )
-                        .padding(10)
-                }
-            }
+        ZStack(alignment: .topTrailing) {
+            GoogleMapView(shopsMapVM: viewModel)
+                .padding(5)
+                .background(Color.blue)
             
+            Button(action: {
+                self.viewModel.loadShops()
+            }) {
+                Text("Reload")
+                    .foregroundColor(.white)
+                    .padding(10)
+                    .background(Color.purple)
+                    .cornerRadius(13)
+                    .overlay(RoundedRectangle(cornerRadius: 13)
+                                .stroke(Color.black, lineWidth: 2)
+                    )
+                    .padding(10)
+            }
+            if viewModel.isShowingProgress {
+                VStack {
+                    Spacer()
+                    CustomedProgress()
+                    Spacer()
+                }
+                
+            }
             NavigationLink(destination: ShopDetailView(viewModel: .init(mapVM: self.viewModel)),
                            isActive: $viewModel.isShopSelected) {
                 EmptyView()
