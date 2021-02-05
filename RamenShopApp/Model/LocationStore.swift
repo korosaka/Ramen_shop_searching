22ebb146267756cdf7ecac208a6675c9a8516572
koro <koro.nn99@gmail.com>
2021-02-05 14:54:54 -0800

    set camera center to user location when searching mode

---
+++ RamenShopApp/Model/LocationStore.swift
@@ -0,0 +1,12 @@
+//
+//  LocationStore.swift
+//  RamenShopApp
+//
+//  Created by Koro Saka on 2021-02-05.
+//  Copyright © 2021 Koro Saka. All rights reserved.
+//
+
+import Foundation
+class LocationStore {
+    
+}
