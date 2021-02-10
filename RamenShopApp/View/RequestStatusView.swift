1e6515aae6ccaa9ee84545802400d233a7c2e98a
koro <koro.nn99@gmail.com>
2021-02-09 17:36:12 -0800

    modified request status page design

--- RamenShopApp/View/RequestStatusView.swift
+++ RamenShopApp/View/RequestStatusView.swift
@@ -14,27 +14,35 @@ struct RequestStatusView: View {
         ZStack {
             BackGroundView()
             ScrollView(.vertical) {
-                Spacer().frame(height: 15)
-                Text("Your Request Status")
-                    .middleTitleStyle()
-                if viewModel.hasRequest {
-                    Spacer().frame(height: 30)
-                    RequestInfo()
-                        .sidePadding(size: 15)
-                    Spacer().frame(height: 20)
-                    RemoveButton()
-                        .sidePadding(size: 15)
-                    Spacer().frame(height: 30)
-                    Text(viewModel.annotation)
-                        .bold()
-                        .foregroundColor(.navy)
-                        .padding(10)
-                        .sidePadding(size: 10)
-                    Spacer().frame(height: 50)
-                } else {
-                    Spacer()
-                    Text("You have no request now").foregroundColor(.white)
-                    Spacer()
+                ZStack(alignment: .topTrailing) {
+                    VStack(spacing: 0) {
+                        Spacer().frame(height: 15)
+                        Text("Request Status")
+                            .middleTitleStyle()
+                        if viewModel.hasRequest {
+                            Spacer().frame(height: 30)
+                            RequestInfo()
+                                .sidePadding(size: 15)
+                            Spacer().frame(height: 20)
+                            RemoveButton()
+                                .sidePadding(size: 15)
+                            Spacer().frame(height: 30)
+                            Text(viewModel.annotation)
+                                .bold()
+                                .foregroundColor(.navy)
+                                .padding(10)
+                                .sidePadding(size: 10)
+                            Spacer().frame(height: 50)
+                        } else {
+                            Spacer()
+                            Text("You have no request now")
+                                .foregroundColor(.white)
+                            Spacer()
+                        }
+                    }
+                    .wideStyle()
+                    
+                    ReloadStatusButton().padding(10)
                 }
             }
             if viewModel.isShowingProgress {
@@ -91,8 +99,6 @@ struct ReviewStatus: View {
                     .padding(5)
                 Spacer().frame(height: 30)
             }
-            ReloadStatusButton()
-            Spacer().frame(height: 5)
         }
     }
 }
